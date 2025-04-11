0f3f7bd08c5309598a6d541123a12b2048a7e39d
Paulina Bartczak <paulinabartczak2@onet.pl>
2025-04-11 21:59:43 +0200

    Page layout, script functions created.

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let result = 0;
+
+function displayResult() {
+    // TODO: implement
+    //document.getElementById("resultInput").value = "1";
+}
+
+function getRandomNumber() {
+    // TODO: implement
+    
+}
+
